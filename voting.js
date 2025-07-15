80b26d8c0e17295cc56f678469ce9c8a72f08c4e
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2025-07-15 04:44:52 -0700

    working on functionality

---
+++ voting.js
@@ -0,0 +1,8 @@
+function submit(){
+    console.log("3");
+    alert("onclick working");
+    // const data = document.getElementById("formData");
+    // console.log(data);
+    // alert(`this is ${data}`);
+}
+
